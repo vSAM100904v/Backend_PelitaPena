d6a333804d2c39ccade3ca7a795652191a874fd0
vSAM100904v <149343649+vSAM100904v@users.noreply.github.com>
2025-05-09 14:38:37 +0700

    Use PORT env var for listening in main.go

--- main.go
+++ main.go
@@ -4,17 +4,35 @@ import (
 	"backend-pedika-fiber/database"
 	"backend-pedika-fiber/migration"
 	"backend-pedika-fiber/routes"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	// Buat instance Fiber
 	app := fiber.New()
+
+	// Inisialisasi database dan jalankan migrasi
 	database.GetDBInstance()
 	migration.RunMigration()
+
+	// Atur routing
 	routes.SetAuthRoutes(app)
 	routes.SetAdminRoutes(app)
 	routes.SetMasyarakatRoutes(app)
 	routes.RoutesWithOutLogin(app)
-	app.Listen(":8080")
+
+	// Ambil PORT dari environment variable (fallback ke 8080)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := fmt.Sprintf(":%s", port)
+	log.Printf("Listening on %s...", addr)
+
+	// Jalankan aplikasi di PORT yang ditentukan
+	log.Fatal(app.Listen(addr))
 }
