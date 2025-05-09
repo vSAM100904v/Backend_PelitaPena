889bc3a8b23433676a20a55f355295e38cc690f9
vSAM100904v <149343649+vSAM100904v@users.noreply.github.com>
2025-05-09 09:39:56 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,38 +1,20 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
-
 	"backend-pedika-fiber/database"
 	"backend-pedika-fiber/migration"
 	"backend-pedika-fiber/routes"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	// 1. Load .env (jika ada)
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, using system env vars")
-	}
-
-	// 2. Inisialisasi Fiber
 	app := fiber.New()
-
-	// 3. Koneksi DB, migrasi, routes
 	database.GetDBInstance()
 	migration.RunMigration()
 	routes.SetAuthRoutes(app)
 	routes.SetAdminRoutes(app)
 	routes.SetMasyarakatRoutes(app)
 	routes.RoutesWithOutLogin(app)
-
-	// 4. Start server di PORT dari env
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3060" // fallback
-	}
-	app.Listen(":" + port)
+	app.Listen(":8080")
 }
