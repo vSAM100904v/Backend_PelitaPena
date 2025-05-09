f3b113a5f1b51d575553db13ede6889763a82b92
vSAM100904v <149343649+vSAM100904v@users.noreply.github.com>
2025-05-09 15:05:48 +0700

    Integrasi Go‑Backend via HTTP

--- database/database.go
+++ database/database.go
@@ -27,11 +27,13 @@ func getDB() *sql.DB {
 			log.Fatal("Error loading .env file")
 		}
 
-		db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",
+		db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 			os.Getenv("DB_USERNAME"),
 			os.Getenv("DB_PASSWORD"),
-			os.Getenv("DB_URL"),
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
 			os.Getenv("DB_DATABASE")))
+
 		if err != nil {
 			log.Fatal(err)
 		}
